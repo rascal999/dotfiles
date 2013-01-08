298e11cfee7d9819b4a1c237dade499098ccf55e
Aidan Marlin <aidan.marlin@gmail.com>
2013-01-08 12:06:09 +0000

    Add ~/.vimrc

---
+++ .vimrc
@@ -0,0 +1,7 @@
+syntax on
+set tabstop=3
+set shiftwidth=3
+set expandtab
+set magic
+nnoremap <f3> :bn<cr>
+nnoremap <f2> :bN<cr>
--- scripts/cp_dotfiles.sh
+++ scripts/cp_dotfiles.sh
@@ -1,9 +1,10 @@
 #/bin/bash
 
-cp -rf ~/.xinitrc ./
-cp -rf ~/.Xdefaults ./
-cp -rf ~/.bashrc ./
-cp -rf ~/.conkyrc ./
-cp -rf ~/.i3/config ./.i3/
-cp -rf ~/.i3/pacman_updates.sh ./.i3/
-cp -rf ~/.i3/conky-i3bar.sh ./.i3/
+cp -irf ~/.xinitrc ./
+cp -irf ~/.Xdefaults ./
+cp -irf ~/.bashrc ./
+cp -irf ~/.conkyrc ./
+cp -irf ~/.i3/config ./.i3/
+cp -irf ~/.i3/pacman_updates.sh ./.i3/
+cp -irf ~/.i3/conky-i3bar.sh ./.i3/
+cp -irf ~/.vimrc ./
