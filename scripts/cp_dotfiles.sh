a2de9c867f147eab3eece1fa3a7786744a8d7acd
Aidan Marlin <aidan.marlin+github@gmail.com>
2013-06-03 09:37:15 +0000

    Improved cp_dotfiles script

--- scripts/cp_dotfiles.sh
+++ scripts/cp_dotfiles.sh
@@ -1,12 +1,14 @@
 #/bin/bash
 
-cp -irf ~/.xinitrc ./
-cp -irf ~/.Xdefaults ./
-cp -irf ~/.bashrc ./
-cp -irf ~/.conkyrc ./
-cp -irf ~/.i3/config ./.i3/
-cp -irf ~/.i3/pacman_updates.sh ./.i3/
-cp -irf ~/.i3/conky-i3bar.sh ./.i3/
-cp -irf ~/.vimrc ./
-cp -irf ~/.gitignore_global ./
-cp -rf ~/.vim ./
+CWD=$(pwd)
+
+cp -irf $CWD/.xinitrc ~/
+cp -irf $CWD/.Xdefaults ~/
+cp -irf $CWD/.bashrc ~/
+cp -irf $CWD/.conkyrc ~/
+cp -irf $CWD/.i3/config ~/.i3/
+cp -irf $CWD/.i3/pacman_updates.sh ~/.i3/
+cp -irf $CWD/.i3/conky-i3bar.sh ~/.i3/
+cp -irf $CWD/.vimrc ~/
+cp -irf $CWD/.gitignore_global ~/
+cp -rf $CWD/.vim ~/
